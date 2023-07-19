59fbc871295d529c67cb7f9d0c2fcedc3f7d5d92
Ryota Suzuki <suzuki@ef-prime.com>
2023-07-20 01:42:12 +0900

    initial commit

---
+++ R/collect.R
@@ -0,0 +1,72 @@
+collect <- function(
+    pkgs,
+    date,
+    outdir,
+    dependencies = c("Imports", "Depends"),
+    repos = "https://cloud.r-project.org",
+    skip_installed = FALSE,
+    skip_recommended = TRUE,
+    overwrite = FALSE
+) {
+
+  # create dir if not exists
+  if(file.exists(outdir)) {
+    if(!file.info(outdir)$isdir) {
+      stop("outdir should be a directory path")
+    } else if(length(list.files(outdir)) > 0 && !overwrite){
+      stop("outdir is not empty. Set overwrite = TRUE to force overwriting")
+    }
+  } else {
+    dir.create(outdir, recursive = TRUE)
+  }
+
+  # latest packages
+  pkg_by_date_url <- paste0(repos, "/web/packages/available_packages_by_date.html")
+  pkg_latest <- read_html(pkg_by_date_url) %>%
+    html_element("table") %>%
+    html_table() %>%
+    select(Package, Date)
+
+  # installed packages
+  pkg_inst <- installed.packages() %>%
+    as_tibble() %>%
+    select(Package, Version, Priority)
+
+  # packages to be excluded
+  pkg_exclude <- if(skip_installed) {
+    pkg_installed
+  } else {
+    if(skip_recommended) {
+      pkg_inst %>%
+        filter(Priority %in% c("base", "recommended"))
+    } else {
+      pkg_inst %>%
+        filter(Priority == "base")
+    }
+  }
+
+  # call inner function
+  result <- .collect(
+    pkgs = pkgs,
+    date = date,
+    outdir = outdir,
+    dependencies = dependencies,
+    repos = repos,
+    pkg_latest = pkg_latest,
+    exclude = pkg_exclude$Package)
+
+  script <- c(
+    paste0("# virtual snapshot on ", date, " for ", paste(pkgs, collapse = ", ")),
+    result %>%
+      mutate(code = paste0('install.packages("', file, '", repos = NULL, type = "source")')) %>%
+      pull(code)
+  )
+
+  write(script, file.path(outdir, "install.R"))
+  capture.output(
+    result %>% as.data.frame %>% print,
+    file = file.path(outdir, "log_collect.txt")
+  )
+
+  return(result)
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# wayback
+
+## Overview
+
+`wayback` works like a virtual CRAN snapshot for source packages.
+It automatically downloads `tar.gz` files with dependencies,
+all of which were available on a specified day. It also creates
+an R script file to install all the downloaded packages locally.
+
+## Install
+
+```
+devtools::install_github("r-suzuki/wayback")
+```
+
+## Example
+To collect `ranger` package and it's dependent packages on the date `2023-03-01`:
+
+```
+collect(pkgs = "ranger", date = "2023-03-01", outdir = "outdir")
+```
+
+It downloads the source `tar.gz` files which were available on the day.
+Here is an example of log file:
+
+```
+  package                 file       date    type
+1    Rcpp   Rcpp_1.0.10.tar.gz 2023-01-22 archive
+2  ranger ranger_0.14.1.tar.gz 2022-06-18 archive
+```
+
+It also creates an R script named `install.R`:
+
+```
+# virtual snapshot on 2023-03-01 for ranger
+install.packages("Rcpp_1.0.10.tar.gz", repos = NULL, type = "source")
+install.packages("ranger_0.14.1.tar.gz", repos = NULL, type = "source")
+```
+
+You can install all the packages with this script.
